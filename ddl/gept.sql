01bf1f4b33c80ee01e5dc00b9ddedc7542d27fa8
ndufrane <dufrane.nicolas@gmail.com>
2017-12-11 11:23:26 +0100

    fuck

---
+++ ddl/gept.sql
@@ -0,0 +1,30 @@
+-- Table: public.gept
+
+
+DROP TABLE IF EXISTS public.gept;
+DROP INDEX IF EXISTS public.gept_the_geom_gist;
+DROP SEQUENCE IF EXISTS public.gept_gid_seq;
+
+CREATE SEQUENCE public.gept_gid_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+CREATE TABLE public.gept
+(
+    gid integer NOT NULL DEFAULT nextval('gept_gid_seq'::regclass),
+    geptty character varying(2),
+    geptna character varying(50),
+    sheet character varying(18),
+    the_geom geometry,
+    CONSTRAINT gept_pkey PRIMARY KEY (gid),
+    CONSTRAINT enforce_dims_the_geom CHECK (st_ndims(the_geom) = 2),
+    CONSTRAINT enforce_geotype_the_geom CHECK (geometrytype(the_geom) = 'POINT'::text OR the_geom IS NULL),
+    CONSTRAINT enforce_srid_the_geom CHECK (st_srid(the_geom) = 31370)
+);
+
+CREATE INDEX gept_the_geom_gist
+    ON public.gept USING gist
+    (the_geom);
