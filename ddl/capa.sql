98894d760e232350b01a9c88cff9c7d8305ae425
ndufrane <dufrane.nicolas@gmail.com>
2018-02-26 10:26:47 +0100

    Fix CAPA. ExponentLetter and ExponentNumber are now splitted into exposant and puissance

--- ddl/capa.sql
+++ ddl/capa.sql
@@ -24,7 +24,7 @@ CREATE TABLE public.capa
     radical integer,
     exposant character varying(5),
     bis character varying(2),
-    puissance character varying(1),
+    puissance character varying(3),
     CONSTRAINT enforce_dims_the_geom CHECK (st_ndims(the_geom) = 2),
     --CONSTRAINT enforce_geotype_the_geom CHECK (geometrytype(the_geom) = 'MULTIPOLYGON'::text OR the_geom IS NULL),
     CONSTRAINT enforce_srid_the_geom CHECK (st_srid(the_geom) = 31370)
--- import.py
+++ import.py
@@ -150,8 +150,8 @@ map.to_csv (path_to_data + '/o_map.csv', sep='|', columns=['capakey', 'pe', 'adr
 # --------------- CAPA -------------------
 print ('Génération de B_CAPA.shp')
 map.rename (columns = {'divCad':'da', 'primaryNumber':'radical','bisNumber':'bis'}, inplace = True)
-map['exposant'] = map['exponentLetter'] + ' ' + map['exponentNumber']
-capa['puissance'] = ''
+map['exposant'] = map['exponentLetter']
+capa['puissance'] = map['exponentNumber']
 capa =  capa.merge (map.loc[:,['CAPAKEY','da','section','radical','exposant','bis']].drop_duplicates(subset = ['CAPAKEY']), how = 'left', left_on = 'CAPAKEY', right_on = 'CAPAKEY' )
 capa.to_file (path_to_data + "/OB_CaPa.shp")
 print ('Procédure terminée\n')
