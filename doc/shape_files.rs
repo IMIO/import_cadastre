f3e1996c3744912eb9b2c7fdc3ad3dcc479a7d08
Joël Lambillotte <joel@imio.be>
2018-11-23 09:20:44 +0100

    Documantation 2018

---
+++ doc/shape_files.rs
@@ -0,0 +1,115 @@
+
+Cadastral parcels
+-----------------
+
+OB_CaPa.shp
+['CAPAKEY', 'CAPATY', 'SHAPE_AREA', 'SHEET', 'puissance', 'da', 'section', 'radical', 'exposant', 'bis', 'geometry']
+
+B_CaPa.shp
+['CAPAKEY', 'CAPATY', 'SHAPE_AREA', 'SHEET', 'geometry']
+
+# 2018
+ Bpn_CaPa.shp
+ ['RecId', 'CaPaKey', 'Type', 'CaSeKey', 'FiscSitId', 'UpdDate', 'Shape_area', 'geometry']
+
+Cadastral building
+------------------
+
+B_CaBu.shp
+['CAPAKEY', 'CAPATY', 'SHAPE_AREA', 'SHEET', 'geometry']
+
+# 2018
+Bpn_CaBu.shp
+['RecId', 'Type', 'FiscSitId', 'UpdDate', 'Shape_area', 'geometry']
+
+Cadastral number (shortened CaPaKey)
+------------------------------------
+
+B_CaNu.shp
+['CAPAKEY', 'CAPATY', 'SHAPE_AREA', 'SHEET', 'geometry']
+
+# 2018
+B_pt_Canu.shp
+['CaPaKey', 'UpdDate', 'CaNuTx', 'geometry']
+
+General line : Boundary
+-----------------------
+
+B_GeLi.shp
+['CAPAKEY', 'CAPATY', 'SHAPE_AREA', 'SHEET', 'geometry']
+
+# 2018
+Water surface
+Bli_WaSu.shp
+
+Easement zone
+Bli_EaZo.shp
+['RecId', 'Type', 'FiscSitId', 'UpdDate', 'Shape_len', 'geometry']
+
+Toponomy names
+Bli_ToNa.shp
+['RecId', 'Type', 'Name', 'FiscSitId', 'UpdDate', 'Shape_len', 'geometry']
+
+
+Water
+-----
+
+Plan/B_GePn.shp
+['CAPAKEY', 'CAPATY', 'SHAPE_AREA', 'SHEET', 'geometry']
+
+# 2018
+Water Surface
+Bpn_WaSu.shp
+['RecId', 'Name', 'FiscSitId', 'UpdDate', 'Shape_area', 'geometry']
+
+
+General line : Border Stone
+---------------------------
+
+Plan/B_GePt.shp
+['CAPAKEY', 'CAPATY', 'SHAPE_AREA', 'SHEET', 'geometry']
+
+# 2018
+Bpt_PrSt
+['RecId', 'Type', 'Name', 'Descriptio', 'OnBoundary', 'FiscSitId', 'UpdDate', 'geometry']
+
+Information line : Survey line 
+-------------------------------
+
+Plan/B_InLi.shp
+
+None
+
+Information Point : Warning
+---------------------------
+
+Plan/B_InPt.shp
+
+None
+
+
+Toponomy Line : railroad, water, street
+----------------------------------------
+Plan/B_ToLi.shp
+['CAPAKEY', 'CAPATY', 'SHAPE_AREA', 'SHEET', 'geometry']
+
+# 2018
+Bli_ToNa
+['RecId', 'Type', 'Name', 'FiscSitId', 'UpdDate', 'Shape_len', 'geometry']
+
+Toponomy Point : railroad, water, street
+----------------------------------------
+
+Plan/B_ToPt.shp
+['CAPAKEY', 'CAPATY', 'SHAPE_AREA', 'SHEET', 'geometry']
+
+# 2018
+Bpt_ToNa
+['RecId', 'Type', 'Name', 'FiscSitId', 'UpdDate', 'geometry']
+
+
+???
+Plan/A_AdMu.shp
+
+
+
